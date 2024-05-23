bfb49f14365dd76044b0cc6af8e15907bcb7cb96
Anandh-23 <92389085+Anandh-23@users.noreply.github.com>
2024-05-23 12:08:32 +0530

    Revert "update exception handling in user profile"

--- userProfile/src/test/java/com/userprofile/UserProfileApplicationTests.java
+++ userProfile/src/test/java/com/userprofile/UserProfileApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import com.userprofile.repository.UserProfileRepository;
-import com.userprofile.services.UserServiceInterface;
+import com.userprofile.services.interfaces.UserServiceInterface;
 
 @SpringBootTest
 class UserProfileApplicationTests {
